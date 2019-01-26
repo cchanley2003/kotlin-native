b82712d39d3d2d22e67989fb5b299aef413ad9bd
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2019-01-26 05:40:37 +0300

    Several performance tunings.

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/DeclarationTable.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/DeclarationTable.kt
@@ -6,17 +6,18 @@ import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.declarations.impl.IrAnonymousInitializerImpl
 import org.jetbrains.kotlin.ir.descriptors.IrBuiltIns
 
-fun <K, V> MutableMap<K, V>.putOnce(k:K, v: V): Unit {
-    assert(!this.containsKey(k) || this[k] == v) {
-        "adding $v for $k, but it is already ${this[k]} for $k"
+fun <K, V> MutableMap<K, V>.putOnce(k:K, v: () -> V): Unit {
+
+    assert(!this.containsKey(k)/* || this[k] == v*/) {
+        "adding ${v()} for $k, but it is already ${this[k]} for $k"
     }
-    this.put(k, v)
+    this.put(k, v())
 }
 
 class DescriptorTable {
     private val descriptors = mutableMapOf<DeclarationDescriptor, Long>()
     fun put(descriptor: DeclarationDescriptor, uniqId: UniqId) {
-        descriptors.putOnce(descriptor, uniqId.index)
+        descriptors.getOrPut(descriptor) { uniqId.index }
     }
     fun get(descriptor: DeclarationDescriptor) = descriptors[descriptor]
 }
@@ -31,30 +32,29 @@ class DeclarationTable(val builtIns: IrBuiltIns, val descriptorTable: Descriptor
 
     init {
         builtIns.knownBuiltins.forEach {
-            table.put(it, UniqId(currentIndex ++, false))
+            table.put(it, UniqId(currentIndex++, false))
         }
     }
 
-    fun uniqIdByDeclaration(value: IrDeclaration): UniqId {
-        val index = table.getOrPut(value) {
+    fun uniqIdByDeclaration(value: IrDeclaration): UniqId = table.getOrPut(value) {
+        val index = if (value.origin == IrDeclarationOrigin.FAKE_OVERRIDE ||
+            !value.isExported()
+            || value is IrVariable
+            || value is IrTypeParameter
+            || value is IrValueParameter
+            || value is IrAnonymousInitializerImpl
+        ) {
 
-            if (value.origin == IrDeclarationOrigin.FAKE_OVERRIDE ||
-                !value.isExported()
-                    || value is IrVariable
-                    || value is IrTypeParameter
-                    || value is IrValueParameter
-                    || value is IrAnonymousInitializerImpl
-            ) {
-
-                UniqId(currentIndex++, true)
-            } else {
-                UniqId(value.uniqIdIndex, false)
-            }
+            UniqId(currentIndex++, true)
+        } else {
+            UniqId(value.uniqIdIndex, false)
         }
 
-        debugIndex.put(index, "${if (index.isLocal) "" else value.uniqSymbolName()} descriptor = ${value.descriptor}")
+        debugIndex.putOnce(index) {
+            "${if (index.isLocal) "" else value.uniqSymbolName()} descriptor = ${value.descriptor}"
+        }
 
-        return index
+        index
     }
 }
 
